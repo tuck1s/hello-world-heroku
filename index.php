fa58c23118e6ab10aa20a119e59324c08ec13840
tuck1s <steve.tuck@messagesystems.com>
2017-11-15 13:10:39 +0000

    Added composer.json

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?PHP
-    phpinfo();
+
+echo("PHP version" + phpversion());
+
+$factorial = 1;
+for ($x=1; $x<=69; $x++)
+{
+    $factorial = $factorial * $x;
+    echo "<br> $x! = $factorial";
+
+}
 ?>
